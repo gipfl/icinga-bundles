0308f7586dc84916dfb4083c5ab0d8a62e0360ab
Thomas Gelf <thomas@gelf.net>
2025-09-03 03:14:20 +0200

    bin/refresh-provided-libs: upgrade, improvements

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
-$modules = [
-    'ipl'         => 'v0.5.0',
-    'reactbundle' => 'v0.8.0',
+$repos = [
+    // legacy:
+    // 'reactbundle' => 'https://github.com/Icinga/icingaweb2-module-reactbundle/archive/refs/tags/v0.8.0.tar.gz',
+    // 'ipl'         => 'https://github.com/Icinga/icingaweb2-module-ipl/archive/refs/tags/v0.5.0.tar.gz',
+    'icinga-php-library'    => 'https://github.com/Icinga/icinga-php-library/archive/refs/tags/v0.13.1.tar.gz',
+    'icinga-php-thirdparty' => 'https://github.com/Icinga/icinga-php-thirdparty/archive/refs/tags/v0.12.1.tar.gz',
 ];
